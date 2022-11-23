17092f4815554176668c591a7d8544eb6f1292ff
Alan Race <alan.race@uni-marburg.de>
2022-11-23 10:01:54 +0100

    Polygons from pixel annotations now fit pixels exactly

--- src/data.rs
+++ src/data.rs
@@ -9,7 +9,8 @@ use bevy_prototype_lyon::prelude::{
     DrawMode, FillMode, GeometryBuilder, PathBuilder, StrokeMode, StrokeOptions,
 };
 use image::GrayImage;
-use imageproc::contours::find_contours;
+use imageproc::contours::{find_contours, Contour};
+use imageproc::point::Point;
 use rand::Rng;
 use std::fs::File;
 use tiff::decoder::Decoder;
@@ -31,6 +32,201 @@ pub enum DataCommand {
     CloseData(Entity),
     IMCEvent(IMCEvent),
     LoadCellData(Entity, PathBuf),
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, PartialOrd)]
+enum Corner {
+    BottomRight,
+    BottomLeft,
+    TopLeft,
+    TopRight,
+}
+
+impl Corner {
+    fn clockwise(&self) -> Self {
+        match self {
+            Corner::BottomRight => Corner::BottomLeft,
+            Corner::BottomLeft => Corner::TopLeft,
+            Corner::TopLeft => Corner::TopRight,
+            Corner::TopRight => Corner::BottomRight,
+        }
+    }
+    fn anticlockwise(&self) -> Self {
+        match self {
+            Corner::BottomRight => Corner::TopRight,
+            Corner::BottomLeft => Corner::BottomRight,
+            Corner::TopLeft => Corner::BottomLeft,
+            Corner::TopRight => Corner::TopLeft,
+        }
+    }
+}
+
+fn corner_coord(point: &Point<u32>, pixel_size: f32, corner: Corner) -> Vec2 {
+    match corner {
+        Corner::BottomRight => Vec2::new(
+            point.x as f32 * pixel_size + pixel_size,
+            point.y as f32 * pixel_size + pixel_size,
+        ),
+        Corner::BottomLeft => Vec2::new(
+            point.x as f32 * pixel_size,
+            point.y as f32 * pixel_size + pixel_size,
+        ),
+        Corner::TopLeft => Vec2::new(point.x as f32 * pixel_size, point.y as f32 * pixel_size),
+        Corner::TopRight => Vec2::new(
+            point.x as f32 * pixel_size + pixel_size,
+            point.y as f32 * pixel_size,
+        ),
+    }
+}
+
+fn process_boundaries_anticlockwise(contour: &Contour<u32>, pixel_size: f32) -> Vec<Vec2> {
+    let mut last_corner = Corner::BottomLeft;
+
+    let mut coords = Vec::with_capacity(contour.points.len());
+
+    // Add in the first point (top left of the first pixel)
+    // coords.push(corner_coord(
+    //     &contour.points[0],
+    //     pixel_size,
+    //     Corner::TopLeft,
+    // ));
+
+    for (index, point) in contour.points.iter().enumerate() {
+        let next_point = if index + 1 < contour.points.len() {
+            &contour.points[index + 1]
+        } else {
+            &contour.points[0]
+        };
+
+        let (target_corner, next_corner) = if next_point.x < point.x && next_point.y < point.y {
+            // Next point is up and left
+            (Corner::TopLeft, Corner::BottomRight)
+        } else if next_point.x < point.x && next_point.y == point.y {
+            // Left
+            (Corner::TopLeft, Corner::TopRight)
+        } else if next_point.x < point.x && next_point.y > point.y {
+            // Bottom left
+            (Corner::BottomLeft, Corner::TopRight)
+        } else if next_point.x == point.x && next_point.y > point.y {
+            // Bottom
+            (Corner::BottomLeft, Corner::TopLeft)
+        } else if next_point.x > point.x && next_point.y > point.y {
+            // Bottom right
+            (Corner::BottomRight, Corner::TopLeft)
+        } else if next_point.x > point.x && next_point.y == point.y {
+            // Right
+            (Corner::BottomRight, Corner::BottomLeft)
+        } else if next_point.x > point.x && next_point.y < point.y {
+            // Upper right
+            (Corner::TopRight, Corner::BottomLeft)
+        } else if next_point.x == point.x && next_point.y < point.y {
+            // Up
+            (Corner::TopRight, Corner::BottomRight)
+        } else {
+            // next_point == point
+            (Corner::TopLeft, Corner::BottomLeft)
+        };
+
+        if index > 0 {
+            while last_corner != target_corner {
+                last_corner = last_corner.anticlockwise();
+
+                coords.push(corner_coord(point, pixel_size, last_corner));
+            }
+        } else {
+            coords.push(corner_coord(point, pixel_size, last_corner));
+        }
+
+        last_corner = next_corner;
+    }
+
+    // Finish adding in necessary points for the first point (also for cases where only one point )
+    while last_corner != Corner::TopLeft {
+        last_corner = last_corner.anticlockwise();
+        coords.push(corner_coord(&contour.points[0], pixel_size, last_corner));
+    }
+
+    coords
+}
+
+fn process_boundaries_clockwise(contour: &Contour<u32>, pixel_size: f32) -> Vec<Vec2> {
+    let mut last_corner = Corner::BottomLeft;
+
+    let mut coords = Vec::with_capacity(contour.points.len());
+
+    let mut moved = false;
+
+    for (index, point) in contour.points.iter().enumerate() {
+        let mut next_point;
+
+        let (target_corner, next_corner, diagonal) = if index + 1 < contour.points.len() {
+            // Look where the next pixel is
+            next_point = &contour.points[index + 1];
+
+            let (target_corner, next_corner, diagonal) =
+                if next_point.x < point.x && next_point.y < point.y {
+                    // Upper left
+                    (Corner::TopLeft, Corner::BottomRight, true)
+                } else if next_point.x < point.x && next_point.y == point.y {
+                    // Left
+                    (Corner::BottomLeft, Corner::BottomRight, false)
+                } else if next_point.x < point.x && next_point.y > point.y {
+                    // Bottom left
+                    (Corner::BottomLeft, Corner::TopRight, true)
+                } else if next_point.x == point.x && next_point.y > point.y {
+                    // Bottom
+                    (Corner::BottomRight, Corner::TopRight, false)
+                } else if next_point.x > point.x && next_point.y > point.y {
+                    // Bottom right
+                    (Corner::BottomRight, Corner::TopLeft, true)
+                } else if next_point.x > point.x && next_point.y == point.y {
+                    // Right
+                    (Corner::TopRight, Corner::TopLeft, false)
+                } else if next_point.x > point.x && next_point.y < point.y {
+                    // Upper right
+                    (Corner::TopRight, Corner::BottomLeft, true)
+                } else {
+                    //if next_point.x == point.x && next_point.y < point.y {
+                    // Up
+                    (Corner::TopLeft, Corner::BottomLeft, false)
+                };
+
+            while last_corner != target_corner {
+                coords.push(corner_coord(point, pixel_size, last_corner));
+                last_corner = last_corner.clockwise();
+
+                moved = true;
+            }
+
+            (target_corner, next_corner, diagonal)
+        } else {
+            if contour.points.len() != 1 {
+                break;
+            }
+
+            // If there is only one point, draw it
+            next_point = &contour.points[0];
+            moved = false;
+
+            (Corner::BottomLeft, Corner::BottomLeft, true)
+        };
+
+        if !moved && diagonal {
+            coords.push(corner_coord(point, pixel_size, last_corner));
+
+            last_corner = last_corner.clockwise();
+
+            while last_corner != target_corner {
+                coords.push(corner_coord(point, pixel_size, last_corner));
+                last_corner = last_corner.clockwise();
+            }
+        }
+
+        coords.push(corner_coord(point, pixel_size, last_corner));
+        last_corner = next_corner;
+    }
+
+    coords
 }
 
 fn issue_data_commands(
@@ -97,24 +293,21 @@ fn issue_data_commands(
                 let grey_image = GrayImage::from_raw(width, height, data.clone()).unwrap();
                 let contours = find_contours::<u32>(&grey_image);
 
-                println!("{:?}", contours[0]);
+                println!("{:?}", contours[4]);
+                println!("{:?}", process_boundaries_anticlockwise(&contours[4], 1.0));
 
                 let mut rng = rand::thread_rng();
 
                 for contour in contours.iter() {
                     let mut builder = PathBuilder::new();
 
-                    let first_point = contour.points[0];
-                    builder.move_to(Vec2::new(
-                        first_point.x as f32 + 0.5,
-                        (height - first_point.y) as f32 - 0.5,
-                    ));
+                    let points = process_boundaries_anticlockwise(contour, 1.0);
 
-                    for point in contour.points.iter().skip(1) {
-                        builder.line_to(Vec2::new(
-                            point.x as f32 + 0.5,
-                            (height - point.y) as f32 - 0.5,
-                        ));
+                    let first_point = &points[0];
+                    builder.move_to(Vec2::new(first_point.x, height as f32 - first_point.y));
+
+                    for point in points.iter().skip(1) {
+                        builder.line_to(Vec2::new(point.x, height as f32 - point.y));
                     }
 
                     builder.close();
@@ -134,7 +327,7 @@ fn issue_data_commands(
                             DrawMode::Outlined {
                                 fill_mode: FillMode::color(colour),
                                 outline_mode: StrokeMode {
-                                    options: StrokeOptions::default().with_line_width(0.1),
+                                    options: StrokeOptions::default().with_line_width(0.4),
                                     color: colour, //Color::BLACK,
                                 },
                             },
